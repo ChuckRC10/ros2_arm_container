11adbac3bf002ef5edfb7a8b5628f060c403bf0b
ChuckRC10 <113194872+ChuckRC10@users.noreply.github.com>
2025-08-01 01:50:32 +0000

    bug fixes

--- src/arm_model/setup.py
+++ src/arm_model/setup.py
@@ -7,19 +7,19 @@ package_name = 'arm_model'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    # This should be empty if you have no python modules in a subfolder
+    packages=[], 
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # This line installs all .launch.py files
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        # This line installs all .xacro files from the urdf directory
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
-        # This line installs all .rviz files
+        
+        # Corrected paths for your structure
+        (os.path.join('share', package_name, 'launch'), glob('bringup/launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('description/urdf/*.xacro')),
+        (os.path.join('share', package_name, 'description/ros2_control'), glob('description/ros2_control/*')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
-        # this line installs yaml files in config directory
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
+        (os.path.join('share', package_name, 'config'), glob('bringup/config/*.yaml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -29,7 +29,6 @@ setup(
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
